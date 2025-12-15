a1c1f1dbfecb1f77485997ce9f002af88041c589
Nicolas Voltis <nicolasvoltis@gmail.com>
2025-12-15 18:43:42 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -109,11 +109,6 @@ export default function Layout({ children }) {
       icon: FileText,
     },
     {
-      title: "Ready to Go",
-      url: createPageUrl("ReadyToGo"),
-      icon: CheckCircle2,
-    },
-    {
       title: "Zonas",
       url: createPageUrl("Zonas"),
       icon: MapPin,
