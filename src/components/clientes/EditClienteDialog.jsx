19324c699962af5269d63a4e174b6e911e23e37a
Nicolas Voltis <nicolasvoltis@gmail.com>
2025-12-15 17:38:45 +0000

    File changes

--- src/components/clientes/EditClienteDialog.jsx
+++ src/components/clientes/EditClienteDialog.jsx
@@ -17,7 +17,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { Check, ChevronsUpDown, Loader2 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { toast } from "sonner";
-import { MUNICIPIOS_NAVARRA } from "../../pages/Rutas";
+import { MUNICIPIOS_NAVARRA } from "../../constants/municipios";
 
 export default function EditClienteDialog({ open, onClose, cliente }) {
   const queryClient = useQueryClient();
