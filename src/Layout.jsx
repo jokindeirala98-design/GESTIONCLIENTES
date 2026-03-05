76facade763a0e53f6c65d8e1e7adca924ed29c8
Nicolas Voltis <nicolasvoltis@gmail.com>
2026-03-05 12:28:14 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -186,11 +186,6 @@ export default function Layout({ children }) {
       icon: FileText,
     },
     {
-      title: "Zonas",
-      url: createPageUrl("Zonas"),
-      icon: MapPin,
-    },
-    {
       title: "Clientes",
       url: createPageUrl("Clientes"),
       icon: UsersIcon,
